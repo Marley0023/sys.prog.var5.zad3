cf959c6a83826c010a79436f092ff4b80c9ebca9
Honor <Honor@ENROXES-L>
2024-11-22 23:51:31 +0500

    Добавьте файлы проекта.

---
+++ sys.prog.var5.zad3.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	setlocale(0, "rus");
+	const double pi = 3.14;
+	double alfa = 1.5;
+	double z = 2 * sqrt(2) * cos(alfa) * sin((pi / 4) + 2 * alfa);
+	printf("z = %f\t\n", z);
+}
+
